600c24f078619cf7c60def0cbb3198aa4256f4fb
Lohuama <lohuamalima@MacBook-Air-de-Lohuama.local>
2024-07-01 21:51:29 -0300

    condicional while

--- src/whileLoop.java
+++ src/whileLoop.java
@@ -1,2 +1,23 @@
+import java.util.Scanner;
+
 public class whileLoop {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+        double mediaAvaliacao = 0;
+        double nota = 0;
+        int totalDeNotas = 0;
+
+        while ( nota != -1){
+            System.out.println("Digite sua avaliacao ou -1 pra encerrar: ");
+            nota = leitura.nextDouble();
+
+            if (nota != -1){
+                mediaAvaliacao += nota;
+                totalDeNotas++;
+            }
+
+        }
+
+        System.out.println("media = " + mediaAvaliacao / totalDeNotas);
+    }
 }
